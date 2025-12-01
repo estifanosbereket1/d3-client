4f217c1c461bc0632af74b477427c1685c5d0e5b
Estifanos <estifanosbereket297@gmail.com>
2025-12-01 16:14:09 +0300

    feat: changed config rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       return [
           {
               source:'/api/:path*',
-              destination:`http://localhost:3000/api/:path*`
+              destination:`https://d3.beete-nibab.com/api/:path*`
           }
       ]
   }
